e50f0ddb250b6ce88bf1887859c517ce0258a2e5
Chris Abedi <cabedi02@gmail.com>
2018-06-15 16:53:53 -0400

    added CompanyTree addNode function and print basic tree

--- src/main/java/driver.java
+++ src/main/java/driver.java
@@ -3,14 +3,26 @@ public class driver {
     public static void main(String [] args){
         Node a = new Node();
         Node b = new Node();
-
+        Node c = new Node();
+        Node d = new Node();
 
         CompanyTree ex= new CompanyTree(a);
         b.companyName = "alex";
+        b.id=2222;
         a.companyName="Chris";
+        a.id=1111;
+        c.companyName="nick";
+        c.id=3333;
         a.children.add(b);
+        b.parentId=1111;
+        b.children.add(c);
+        c.parentId=2222;
 
+        d.companyName="chad";
+        d.parentId=1111;
+        d.id=4444;
 
-        ex.printTree(a);
+        ex.addNode(d);
+        ex.printTree();
     }
 }
