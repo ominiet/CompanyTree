2a81aa3c8169e7c7fcde22f448c6354d2884ab75
Chris Abedi <cabedi02@gmail.com>
2018-06-15 15:38:44 -0400

    base node class

--- src/main/java/CompanyTree.java
+++ src/main/java/CompanyTree.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class CompanyTree {
+
+    public class Node{
+        String companyName;
+        int id;
+        ArrayList<Integer> children;
+
+        Node(){
+          this.companyName="unset";
+          this.id=0000;
+          this.children= new ArrayList<Integer>();
+        }
+    }
 }
