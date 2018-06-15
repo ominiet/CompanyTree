9c210832968041226c1abd0ea62266df56185b93
Chris Abedi <cabedi02@gmail.com>
2018-06-15 15:45:59 -0400

    push what you got

--- src/main/java/CompanyTree.java
+++ src/main/java/CompanyTree.java
@@ -1,16 +1,35 @@
 import java.util.ArrayList;
 
+class Node{
+    String companyName;
+    int id;
+    ArrayList<Node> children;
+
+    Node(){
+        this.companyName="unset";
+        this.id=0000;
+        this.children= new ArrayList<Node>();
+    }
+}
 public class CompanyTree {
 
-    public class Node{
-        String companyName;
-        int id;
-        ArrayList<Integer> children;
+    void printTree(Node root){
 
-        Node(){
-          this.companyName="unset";
-          this.id=0000;
-          this.children= new ArrayList<Integer>();
+        for(Node a: root.children){
+            
         }
+
     }
+
+public static void main(String [] args){
+    Node a = new Node();
+    Node b = new Node();
+
+    b.companyName = "alex";
+    a.companyName="Chris";
+    a.children.add(b);
+
+
+}
+
 }
